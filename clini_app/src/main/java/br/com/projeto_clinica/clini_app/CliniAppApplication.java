c094bc849d4568360aea78e0039693d0561d5576
eli1711 <elihofnimariano@gmail.com>
2025-08-22 19:13:48 -0300

    projeto inicial

---
+++ clini_app/src/main/java/br/com/projeto_clinica/clini_app/CliniAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto_clinica.clini_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliniAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliniAppApplication.class, args);
+	}
+
+}
